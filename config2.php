4fe7ced9974273468d263c92a8d7e1a46b5f4448
Tad <tad0616@gmail.com>
2015-06-25 16:42:53 +0800

    新佈景釋出

---
+++ config2.php
@@ -0,0 +1,7 @@
+<?php
+$i                           = 0;
+$theme_config[$i]['name']    = "show_var";
+$theme_config[$i]['text']    = "顯示佈景變數？";
+$theme_config[$i]['desc']    = "是否顯示佈景變數？";
+$theme_config[$i]['type']    = "yesno";
+$theme_config[$i]['default'] = "0";
