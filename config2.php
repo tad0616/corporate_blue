97319fa68739bb66ad90979a0ce34e4b9ed45b34
Tad <tad0616@gmail.com>
2015-06-30 16:03:59 +0800

    重新調整結構以符合新的佈景管理機制

--- config2.php
+++ config2.php
@@ -1,7 +1,7 @@
 <?php
 $i                           = 0;
 $theme_config[$i]['name']    = "show_var";
-$theme_config[$i]['text']    = "顯示佈景變數？";
-$theme_config[$i]['desc']    = "是否顯示佈景變數？";
+$theme_config[$i]['text']    = TF_SHOW_VAR;
+$theme_config[$i]['desc']    = TF_SHOW_VAR_DESC;
 $theme_config[$i]['type']    = "yesno";
 $theme_config[$i]['default'] = "0";
