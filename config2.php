cf482cdc52df108fa815523b7836702f849a145a
Tad <tad0616@gmail.com>
2016-05-10 10:49:11 +0800

    轉為bootstrap3

--- config2.php
+++ config2.php
@@ -5,3 +5,11 @@ $theme_config[$i]['text']    = TF_SHOW_VAR;
 $theme_config[$i]['desc']    = TF_SHOW_VAR_DESC;
 $theme_config[$i]['type']    = "yesno";
 $theme_config[$i]['default'] = "0";
+
+//字型設定
+$i++;
+$theme_config[$i]['name']    = "font_family";
+$theme_config[$i]['text']    = TF_FONT_FAMILY;
+$theme_config[$i]['desc']    = TF_FONT_FAMILY_DESC;
+$theme_config[$i]['type']    = "text";
+$theme_config[$i]['default'] = constant('TF_FONT_FAMILY_DEFAULT');
